04db6f03b9f14c9150ee9a99dcac354a2291c369
Yuna1002 <e2431666@gmail.com>
2023-06-14 22:43:08 +0800

    deploy

---
+++ assets/HomeView-8e84e9cf.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-ce484b6b.js";const o={},s={class:"text-center mb-5"};function n(r,_){return c(),t("h2",s,"首頁")}const f=e(o,[["render",n]]);export{f as default};
